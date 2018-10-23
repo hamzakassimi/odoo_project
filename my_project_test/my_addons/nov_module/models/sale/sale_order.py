dfa1fff374491b91621056b943394a943c877448
Fouzia <you@example.com>
2018-10-23 21:04:44 +0100

    [FIX] reduce total amount of paind invoices

--- my_project_test/my_addons/nov_module/models/sale/sale_order.py
+++ my_project_test/my_addons/nov_module/models/sale/sale_order.py
@@ -23,28 +23,38 @@ class SaleOrder(models.Model):
         for record in self:
             template = self.env.ref('nov_module.email_template_overdrawn_partner_credit')
             try:
-                template.browse(template.id).send_mail(record.id)
-                print('********',                template.browse(template.id).send_mail(record.id)
-)
+                template.send_mail(record.id,force_send=True)
             except Exception as exp:
                 self.env.cr.rollback()
-                logger.error('Failed to Send mail of overdrawn rentals')
-                logger.error(str(exp))
+                _logger.error('Failed to Send mail of overdrawn Partner Credit')
+                _logger.error(str(exp))
+        return True
 
     @api.multi
     def _action_confirm(self):
         for record in self:
             totals_amount = 0.0
+            total_invoices = 0.0
             related_sales = self.env['sale.order'].search([('partner_id','=',record.partner_id.id),('state','=','sale')])
             if record.partner_id.state != 'validated':
                 raise ValidationError(_('you can not confirm a sale order for non validated customer'))
             if related_sales:
-                for so in related_sales:
-                    totals_amount += so.amount_total
+                for order in related_sales:
+                    totals_amount += order.amount_total
+                    if order.invoice_ids:
+                        for invoice in order.invoice_ids:
+                            if invoice.state=='paid':
+                                total_invoices += invoice.amount_total
+                            else:
+                                total_invoices = 0.0
+                    else:
+                        total_invoices = 0.0
             else:
                 totals_amount = 0.0
+            amounts = totals_amount + record.amount_total
+            total_deduced = amounts - total_invoices
             for credit in record.partner_id.partner_credits_ids:
-                if (totals_amount + record.amount_total) <= credit.partner_credit:
+                if total_deduced <= credit.partner_credit:
                     record._notify_email_overdrawn_partner_credit()
                     raise ValidationError(_('the amount total of partner SOs is less than the customer limit :%s , in the comapny %s ') %(str(credit.partner_credit) ,credit.company_id.name))
         return super(SaleOrder, self)._action_confirm()
