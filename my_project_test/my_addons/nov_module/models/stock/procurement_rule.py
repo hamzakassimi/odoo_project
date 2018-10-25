d01ab3cee1d18c7758540477794cc3c10d0ede15
Fouzia <you@example.com>
2018-10-25 23:48:04 +0100

    [WIP]work in progress:AS a sale person I am able to generate automatically purchase order to the supplier configured in corresponding warehouse when confirming SO

---
+++ my_project_test/my_addons/nov_module/models/stock/procurement_rule.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError,UserError
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class ProcurementRule(models.Model):
+    _inherit = 'procurement.rule'
+
+    def _make_po_select_supplier(self, values, suppliers):
+        """ Method intended to be overridden by customized modules to implement any logic in the
+            selection of supplier.
+        """
+        warehouse_supplier = values['warehouse_id']['supplier_id']
+        for supplier in suppliers:
+            if supplier.name.id == warehouse_supplier.id:
+                return supplier
+            else:
+                raise UserError(_('The vendor associated to your warehouse is not configured in vendors of  your products ,Please define it for your products.'))
